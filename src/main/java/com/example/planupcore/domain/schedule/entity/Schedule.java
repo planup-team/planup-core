73ec44c04a503a5a5bfda7dff71e998adb80bd0a
SeungYeop Ryu <n006h3417h@gmail.com>
2026-01-13 11:36:47 +0900

    style: remove unnecessary import

--- src/main/java/com/example/planupcore/domain/schedule/entity/Schedule.java
+++ src/main/java/com/example/planupcore/domain/schedule/entity/Schedule.java
@@ -2,9 +2,7 @@ package com.example.planupcore.domain.schedule.entity;
 
 import jakarta.persistence.*;
 import lombok.Getter;
-import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
